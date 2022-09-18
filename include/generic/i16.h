a266fd2e2d1c9b50d91f01ea900eb8ed7be5f0a6
pseudoc <atlas.yu@canonical.com>
2022-09-18 22:06:47 +0800

    generic headers

---
+++ include/generic/i16.h
@@ -0,0 +1,4 @@
+#ifndef __SPARSE_TABLE_H
+#define __SPARSE_TABLE_TYPE int16_t
+#include "generic.h"
+#endif
---
+++ include/generic/i64.h
@@ -0,0 +1,4 @@
+#ifndef __SPARSE_TABLE_H
+#define __SPARSE_TABLE_TYPE int64_t
+#include "generic.h"
+#endif
